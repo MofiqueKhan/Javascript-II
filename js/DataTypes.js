b0fc0a7b4dc50ddfc0181c35d25c515c326d0635
MofiqueKhan <khanmofique@gmail.com>
2023-05-22 20:42:37 +0530

    DataTypes in js

---
+++ js/DataTypes.js
@@ -0,0 +1,19 @@
+console.log("hello World..!");
+
+/* Data types in js :-
+
+    Primitive :-
+
+1.String 
+2.Number
+3.Boolean
+4.Null
+5.Undefined
+
+    Non-Primitive :-
+
+1.Objects 
+2.Arrays
+
+*/
+
