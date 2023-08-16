871523e5e4a68aac9c8438cec2dba073fb0d8c24
MofiqueKhan <khanmofique@gmail.com>
2023-08-17 04:04:20 +0530

    PoP uPs in js

---
+++ js/alert-promt-confirm.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+
+// alert()
+
+// An alert box is often used if you want to make sure information comes through to the user.
+// When an alert box pops up, the user will have to click "OK" to proceed.
+
+// alert("my msg");
+
+// prompt()
+
+// A prompt box is often used if you want the user to input a value before entering a page.f the user clicks "Cancel" the box returns null.
+
+// let a = prompt("enter your age.");
+// console.log(a);
+
+// confirm()
+
+// A confirm box is often used if you want the user to verify or accept something.If the user clicks "OK", the box returns true. If the user clicks "Cancel", the box returns false.
+
+// let a = confirm("are you sure , you want to delete ?");
+
+// if (a == true) {
+//   console.log("delete it.");
+// } else {
+//   console.log("cancel the delete operation.");
+// }
