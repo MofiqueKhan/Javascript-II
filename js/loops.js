bc4dd4ee78c18ee8f7d767e50d21581cc262f9d4
MofiqueKhan <khanmofique@gmail.com>
2023-08-17 03:08:46 +0530

    loops in js

---
+++ js/loops.js
@@ -0,0 +1,62 @@
+console.log("hello world...!!!");
+
+//                   ----->>>>>   Loops  <<<<<-----
+
+// Loops are used in JavaScript to perform repeated tasks based on a condition.
+
+/*
+
+basically Loops are three types:-
+1. while Loop
+2. do while Loop
+3. for Loop
+
+*/
+
+// while Loop
+
+// let number = 0;
+
+// while (number <= 10) {
+//   console.log(number);
+//   number++;
+// }
+// let counter = 1;
+// let SumOfEven = 0;
+
+// while (SumOfEven <= 100) {
+//   if (SumOfEven % 2 == 0) {
+//     console.log(SumOfEven);
+//   }
+//   SumOfEven++;
+// }
+
+// Dowhile Loop
+
+// let num = 1;
+
+// do{
+//   console.log(num);
+//   num++;
+// }while(num <= 10);
+
+// ------------------------------------------------------------------------------------------
+// do{
+//   num++;
+// }while(num <= 10);
+
+// console.log(num);
+// ------------------------------------------------------------------------------------------
+
+// for loop
+
+// for(let num = 1; num<=10;num++){
+//   console.log(num);
+// }
+
+
+// let sum = 0 
+// for (let i = 0; i <=100 ; i += 2) {
+//   sum += i;
+// }
+// console.log(sum);
